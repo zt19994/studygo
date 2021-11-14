a5c0dad9120d1709c7e1707d9d23ff2d9ba82111
zt19994 <zt191610942@.com>
2021-11-14 20:56:59 +0800

    goroutine socket client 持续发送

--- 03-socket/02-socket-client.go
+++ 03-socket/02-socket-client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
@@ -15,28 +16,31 @@ func main() {
 		fmt.Println("net.Dial error:", err)
 	}
 
-	fmt.Println("Client Server 建立连接成功")
-	sendData := []byte("hello world")
+	for {
+		fmt.Println("Client Server 建立连接成功")
+		sendData := []byte("hello world")
 
-	// 2 write 向服务器发送数据
-	cnt, err := conn.Write(sendData)
-	if err != nil {
-		fmt.Println("conn.Write error:", err)
-		return
+		// 2 write 向服务器发送数据
+		cnt, err := conn.Write(sendData)
+		if err != nil {
+			fmt.Println("conn.Write error:", err)
+			return
+		}
+
+		fmt.Println("Client 发送数据到 Server cnt:", cnt, ", 数据：", sendData)
+
+		// 3 接收服务器返回的数据
+		// 创建buffer，用于接收服务器返回的数据
+		buffer := make([]byte, 1024)
+		cnt, err = conn.Read(buffer)
+		if err != nil {
+			fmt.Println("conn.Read error:", err)
+			return
+		}
+
+		fmt.Println("Client 接收到 Server, cnt:", cnt, ", 数据：", string(buffer[0:cnt]))
+		time.Sleep(1 * time.Second)
 	}
-
-	fmt.Println("Client 发送数据到 Server cnt:", cnt, ", 数据：", sendData)
-
-	// 3 接收服务器返回的数据
-	// 创建buffer，用于接收服务器返回的数据
-	buffer := make([]byte, 1024)
-	cnt, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Println("conn.Read error:", err)
-		return
-	}
-
-	fmt.Println("Client 接收到 Server, cnt:", cnt, ", 数据：", string(buffer[0:cnt]))
 
 	// 4 close 关闭连接
 	err = conn.Close()
